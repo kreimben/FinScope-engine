b934de81c1f069452ee60f949ad73d384ab71d5c
Jehwan Kim <jehwan.kim@kreimben.com>
2024-11-15 16:29:57 +0900

    Optimize dependencies and add flexibility to run locally or on Lambda Details: Updated .gitignore to exclude some additional files Optimized go.mod dependencies: Moved required dependencies to the require section Removed unused dependencies (graphql-go, lib/pq, postgrest-go) Simplified error handling when loading config in internal/config/config.go Added flexibility in main.go to run either locally or on AWS Lambda: Moved config loading to init() Added logic in main() to invoke lambda.Start() when running on Lambda, otherwise run handleRequest() directly Updated go.sum with the latest dependency versions
    Signed-off-by: Jehwan Kim <jehwan.kim@kreimben.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -14,10 +13,7 @@ type Config struct {
 }
 
 func LoadConfig() *Config {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	godotenv.Load()
 
 	return &Config{
 		SupabaseURL:       os.Getenv("SUPABASE_URL"),
--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/kreimben/FinScope-engine/internal/config"
 	"github.com/kreimben/FinScope-engine/internal/crawler"
 )
 
-func handleRequest(ctx context.Context, event json.RawMessage) error {
+var cfg *config.Config
+
+func init() {
 	// Load config.
-	cfg := config.LoadConfig()
+	cfg = config.LoadConfig()
+}
+
+func handleRequest(ctx context.Context, event json.RawMessage) error {
 
 	// Start crawler.
 	crawler.StartFinanceYahooCrawler(cfg)
@@ -25,5 +31,9 @@ func handleRequest(ctx context.Context, event json.RawMessage) error {
 }
 
 func main() {
-	lambda.Start(handleRequest)
+	if os.Getenv("_LAMBDA_SERVER_PORT") != "" {
+		lambda.Start(handleRequest)
+	} else {
+		handleRequest(context.Background(), json.RawMessage{})
+	}
 }
