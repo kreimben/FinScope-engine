0059955b9d511513b54e27357f6692d180d701f0
Jehwan Kim <jehwan.kim@kreimben.com>
2024-11-21 20:01:53 +0900

    feat: Expand EconomicIndicator model with forecast and previous values
    - Added `ForecastValue` and `PreviousValue` fields to the `EconomicIndicator` struct to store additional economic data.
    - Updated struct to include optional fields for more comprehensive economic analysis.
    - Enhanced data model to support a wider range of economic indicators with detailed historical and predictive data.
    
    Signed-off-by: Jehwan Kim <jehwan.kim@kreimben.com>

---
+++ internal/models/economic_indicators.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+)
+
+type EconomicIndicator struct {
+	// ID            string    `json:"id"`      // UUID
+	Name          string    `json:"name"`    // GDP, CPI, etc.
+	Country       string    `json:"country"` // US, EU, etc.
+	ReleaseDate   time.Time `json:"release_date"`
+	ActualValue   float64   `json:"actual_value"`
+	ForecastValue float64   `json:"forecast_value,omitempty"`
+	PreviousValue float64   `json:"previous_value,omitempty"`
+	Unit          string    `json:"unit"` // USD, EUR etc...
+}
