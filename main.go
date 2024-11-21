9ee1c32636ab3d10d90fe1241b366ad821ec3e2c
Jehwan Kim <jehwan.kim@kreimben.com>
2024-11-21 19:58:14 +0900

    feat: Add event-driven execution for economic indicators
    Signed-off-by: Jehwan Kim <jehwan.kim@kreimben.com>

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"os"
+	"slices"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/kreimben/FinScope-engine/internal/config"
 	"github.com/kreimben/FinScope-engine/internal/crawler"
+	"github.com/kreimben/FinScope-engine/internal/economic_indicators"
 	"github.com/kreimben/FinScope-engine/pkg/logging"
 )
 
@@ -19,9 +21,29 @@ func init() {
 	logging.Logger = logging.NewLogger()
 }
 
+type FinScopeEngineEvent struct {
+	Execute []string `json:"execute"` //
+}
+
 func handleRequest(ctx context.Context, event json.RawMessage) error {
-	crawler.StartFinanceYahooCrawler(cfg)
-	crawler.StartBenzingaCrawler(cfg)
+	// unmarshal the event
+	var finScopeEngineEvent FinScopeEngineEvent
+	err := json.Unmarshal(event, &finScopeEngineEvent)
+	if err != nil {
+		return err
+	}
+
+	if slices.Contains(finScopeEngineEvent.Execute, "GDP") {
+		economic_indicators.GatherGDP(cfg)
+	}
+
+	if slices.Contains(finScopeEngineEvent.Execute, "yahoo_finance") {
+		crawler.StartFinanceYahooCrawler(cfg)
+	}
+
+	if slices.Contains(finScopeEngineEvent.Execute, "benzinga") {
+		crawler.StartBenzingaCrawler(cfg)
+	}
 	return nil
 }
 
@@ -29,6 +51,9 @@ func main() {
 	if os.Getenv("_LAMBDA_SERVER_PORT") != "" {
 		lambda.Start(handleRequest)
 	} else {
-		handleRequest(context.Background(), json.RawMessage{})
+		handleRequest(
+			context.Background(),
+			json.RawMessage(`{"execute": ["GDP"]}`),
+		)
 	}
 }
