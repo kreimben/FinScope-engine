23c0e455c1cd6b82fa58fc0bec8ddebbdc00347e
Jehwan Kim <jehwan.kim@kreimben.com>
2024-11-24 15:25:49 +0900

    .
    Signed-off-by: Jehwan Kim <jehwan.kim@kreimben.com>

--- main.go
+++ main.go
@@ -140,7 +140,7 @@ func handleRequest(ctx context.Context, event json.RawMessage) error {
 
 func main() {
 	if os.Getenv("DEBUG") == "true" {
-		handleRequest(context.Background(), json.RawMessage(`{"execute": ["FRED_Gather_Today_Release_Indicator_And_Mark_As_Done"]}`))
+		handleRequest(context.Background(), json.RawMessage(`{"execute": ["DFEDTARU"]}`))
 	} else {
 		lambda.Start(handleRequest)
 	}
