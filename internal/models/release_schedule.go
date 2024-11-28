1ec9e18faa594e9c33dd7bbd42dc1cc2f5e60a20
Jehwan Kim <jehwan.kim@kreimben.com>
2024-11-28 12:55:49 +0900

    feat: enhance ReleaseDate model to support both Supabase and FRED date formats
    - Add new Date field to handle FRED's date format
    - Update UnmarshalJSON to handle both RFC3339 and YYYY-MM-DD date formats
    - Make ReleaseDate field optional with omitempty tag
    - Update debug execution to test FRED_Gather_Release_Schedules
    
    Signed-off-by: Jehwan Kim <jehwan.kim@kreimben.com>

--- internal/models/release_schedule.go
+++ internal/models/release_schedule.go
@@ -8,18 +8,21 @@ import (
 
 /*
  * "[{\"series_id\":\"PPIFIS\",\"release_date\":\"2024-12-12T00:00:00+00:00\",\"done\":false}]"
+ * "{..., \"release_dates\":[{\"release_id\":10,\"date\":\"2024-12-11\"}]}"
  */
 type ReleaseDate struct {
-	SeriesID    string    `json:"series_id,omitempty"`  // GDP
-	ReleaseId   uint16    `json:"release_id,omitempty"` // 53
-	ReleaseDate time.Time `json:"release_date"`
+	SeriesID    string    `json:"series_id,omitempty"`    // GDP
+	ReleaseId   uint16    `json:"release_id,omitempty"`   // 53
+	ReleaseDate time.Time `json:"release_date,omitempty"` // from Supabase
+	Date        time.Time `json:"date,omitempty"`         // from Fred
 }
 
 // Add UnmarshalJSON for ReleaseDate
 func (rd *ReleaseDate) UnmarshalJSON(b []byte) error {
 	type Alias ReleaseDate
 	aux := &struct {
-		ReleaseDate string `json:"release_date"`
+		ReleaseDate string `json:"release_date,omitempty"`
+		Date        string `json:"date,omitempty"`
 		*Alias
 	}{
 		Alias: (*Alias)(rd),
@@ -27,10 +30,18 @@ func (rd *ReleaseDate) UnmarshalJSON(b []byte) error {
 	if err := json.Unmarshal(b, &aux); err != nil {
 		return err
 	}
-	parsedTime, err := time.Parse(time.RFC3339, aux.ReleaseDate)
-	if err != nil {
-		return err
+	if aux.ReleaseDate != "" {
+		parsedTime, err := time.Parse(time.RFC3339, aux.ReleaseDate)
+		if err != nil {
+			return err
+		}
+		rd.ReleaseDate = parsedTime
+	} else if aux.Date != "" {
+		parsedTime, err := time.Parse("2006-01-02", aux.Date)
+		if err != nil {
+			return err
+		}
+		rd.Date = parsedTime
 	}
-	rd.ReleaseDate = parsedTime
 	return nil
 }
